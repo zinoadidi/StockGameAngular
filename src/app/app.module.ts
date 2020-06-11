30a650829d068a136897796016bef4b2020f21da
Zino Adidi <zinoadidi@gmail.com>
2020-06-11 09:14:26 +0300

    WIP: Show list of stocks

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StocksComponent } from './stocks/stocks.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StocksComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/stocks/model/Stock
@@ -0,0 +1,5 @@
+export interface Stock {
+  symbol: string;
+  name: string;
+  price: number;
+}
